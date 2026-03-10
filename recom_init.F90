305786a454f8c6989d3cd68e37c951cd81db94a8
Wilton Jaciel Loch <wilton.loch@gmail.com>
2026-03-10 15:08:58 +0100

    style: fixed identation of recom_init subroutine

--- recom_init.F90
+++ recom_init.F90
@@ -8,57 +8,57 @@ contains
 !
 !
 !_______________________________________________________________________________
-subroutine recom_init(nl, ulevels_nod2D, nlevels_nod2D, geo_coord_nod2D, Z_3d_n,   &
-                      myDim_nod2d, eDim_nod2D, mype, MPI_COMM_FESOM, myDim_elem2D, &
-                      eDim_elem2D, tracers_info, num_tracers, rad)
+    subroutine recom_init(nl, ulevels_nod2D, nlevels_nod2D, geo_coord_nod2D, Z_3d_n,   &
+                          myDim_nod2d, eDim_nod2D, mype, MPI_COMM_FESOM, myDim_elem2D, &
+                          eDim_elem2D, tracers_info, num_tracers, rad)
 
-    use REcoM_declarations
-    use REcoM_GloVar
-    use REcoM_locVar
-    use recom_config
-    use REcoM_ciso
+        use REcoM_declarations
+        use REcoM_GloVar
+        use REcoM_locVar
+        use recom_config
+        use REcoM_ciso
 
-    implicit none
+        implicit none
 
-    integer,        intent(in)                  :: nl, mydim_nod2d, edim_nod2d, mype, num_tracers
-    integer,        intent(in)                  :: mpi_comm_fesom, mydim_elem2d, edim_elem2d
-    real(kind=WP), intent(in)                  :: rad
-    integer,        intent(in), dimension(:)    :: ulevels_nod2d, nlevels_nod2d
-    real(kind=wp),  intent(in), dimension(:, :) :: geo_coord_nod2d, z_3d_n
-    type(tracers_info_type), intent(in) :: tracers_info
+        integer,        intent(in)                  :: nl, mydim_nod2d, edim_nod2d, mype, num_tracers
+        integer,        intent(in)                  :: mpi_comm_fesom, mydim_elem2d, edim_elem2d
+        real(kind=WP), intent(in)                  :: rad
+        integer,        intent(in), dimension(:)    :: ulevels_nod2d, nlevels_nod2d
+        real(kind=wp),  intent(in), dimension(:, :) :: geo_coord_nod2d, z_3d_n
+        type(tracers_info_type), intent(in) :: tracers_info
 
-    !___________________________________________________________________________
-    ! pointer on necessary derived types
-    integer                                 :: n, k, row, nzmin, nzmax, i, id
-    integer                                 :: elem_size, node_size
+        !___________________________________________________________________________
+        ! pointer on necessary derived types
+        integer                                 :: n, k, row, nzmin, nzmax, i, id
+        integer                                 :: elem_size, node_size
 
-    ! After reading tracer namelist - validate actual IDs
-    integer, dimension(num_tracers) :: tracer_id_array
+        ! After reading tracer namelist - validate actual IDs
+        integer, dimension(num_tracers) :: tracer_id_array
 
-    elem_size   = myDim_elem2D + eDim_elem2D
-    node_size   = myDim_nod2D + eDim_nod2D
+        elem_size   = myDim_elem2D + eDim_elem2D
+        node_size   = myDim_nod2D + eDim_nod2D
 
-    call initialize_memory(node_size, nl, num_tracers)
+        call initialize_memory(node_size, nl, num_tracers)
 
-    ! After reading parecomsetup namelist
-    call initialize_tracer_indices
+        ! After reading parecomsetup namelist
+        call initialize_tracer_indices
 
-    ! Validation check here
-    call validate_recom_tracers(num_tracers, mype)
+        ! Validation check here
+        call validate_recom_tracers(num_tracers, mype)
 
-    ! ... populate tracer_id_array from namelist ...
-    !tracer_id_array = tracers%data(1:tracers%num_tracers)%ID
-    tracer_id_array = tracers_info%ids(1:num_tracers)
-    call validate_tracer_id_sequence(tracer_id_array, num_tracers, mype)
+        ! ... populate tracer_id_array from namelist ...
+        !tracer_id_array = tracers%data(1:tracers%num_tracers)%ID
+        tracer_id_array = tracers_info%ids(1:num_tracers)
+        call validate_tracer_id_sequence(tracer_id_array, num_tracers, mype)
 
-    call initialize_tracer_data(num_tracers, tracers_info)
+        call initialize_tracer_data(num_tracers, tracers_info)
 
-!------------------------------------------
+    !------------------------------------------
 
-    call mask_hydrothermal_vents(tracers_info, myDim_nod2D, eDim_nod2D, ulevels_nod2D, nlevels_nod2D, geo_coord_nod2D, Z_3d_n, rad)
-!------------------------------------------
+        call mask_hydrothermal_vents(tracers_info, myDim_nod2D, eDim_nod2D, ulevels_nod2D, nlevels_nod2D, geo_coord_nod2D, Z_3d_n, rad)
+    !------------------------------------------
 
-    call initialization_diagnostics(tracers_info, myDim_nod2D, ulevels_nod2D, nlevels_nod2D, MPI_COMM_FESOM, mype)
+        call initialization_diagnostics(tracers_info, myDim_nod2D, ulevels_nod2D, nlevels_nod2D, MPI_COMM_FESOM, mype)
     end subroutine recom_init
 
     subroutine initialize_memory(node_size, nl, num_tracers)
